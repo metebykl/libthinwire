ec5fc4d1f418c8ec3f88aed8d2a032437cb6559b
Mete <69116434+metebykl@users.noreply.github.com>
2025-09-01 20:27:25 +0300

    feat: add `tw_server_` and `tw_conn_` functions

--- examples/01_basic_server.c
+++ examples/01_basic_server.c
@@ -4,10 +4,43 @@
 #include "../thinwire.h"
 
 int main() {
-  if (tw_server_start() < 0) {
-    fprintf(stderr, "error: failed to initialize server");
-    return 1;
+  tw_server_t server;
+  if (!tw_server_start(&server)) {
+    exit(EXIT_FAILURE);
   };
 
+  tw_log(TW_INFO, "Server listening on port 8080");
+
+  while (1) {
+    tw_conn_t conn;
+    if (!tw_server_accept(&server, &conn)) {
+      continue;
+    }
+
+    tw_log(TW_INFO, "Client connected");
+
+    char buf[2048];
+    ssize_t bytes_read = tw_conn_read(&conn, buf, sizeof(buf) - 1);
+    if (bytes_read <= 0) {
+      tw_log(TW_WARNING, "Failed to read buffer");
+      tw_conn_close(&conn);
+      continue;
+    }
+
+    buf[bytes_read] = '\0';
+    tw_log(TW_INFO, "%s", buf);
+
+    const char *response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 12\r\n"
+        "\r\n"
+        "Hello World!";
+
+    tw_conn_write(&conn, response, strlen(response));
+    tw_conn_close(&conn);
+  }
+
+  tw_server_stop(&server);
   return 0;
 }
--- thinwire.h
+++ thinwire.h
@@ -10,13 +10,47 @@ extern "C" {
 #endif
 
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
+
+#ifdef _WIN32
+#include <winsock2.h>
+#else
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#endif
+
+#ifdef _WIN32
+typedef int socklen_t;
+#define close(fd) closesocket(fd)
+#endif
 
 typedef enum { TW_INFO, TW_WARNING, TW_ERROR } tw_log_level;
 
 TWDEF void tw_log(tw_log_level level, const char *fmt, ...);
 
-TWDEF int tw_server_start();
+#define TW_DEFAULT_PORT 8080
+
+typedef struct {
+  int fd;
+  struct sockaddr_in addr;
+} tw_server_t;
+
+typedef struct {
+  int fd;
+  struct sockaddr_in addr;
+} tw_conn_t;
+
+TWDEF bool tw_server_start(tw_server_t *server);
+TWDEF bool tw_server_stop(tw_server_t *server);
+TWDEF bool tw_server_accept(tw_server_t *server, tw_conn_t *conn);
+
+TWDEF ssize_t tw_conn_read(tw_conn_t *conn, char *buf, size_t len);
+TWDEF ssize_t tw_conn_write(tw_conn_t *conn, const char *buf, size_t len);
+TWDEF void tw_conn_close(tw_conn_t *conn);
 
 #ifdef __cplusplus
 }
@@ -51,9 +85,72 @@ TWDEF void tw_log(tw_log_level level, const char *fmt, ...) {
   fprintf(stream, "\n");
 }
 
-TWDEF int tw_server_start() {
-  tw_log(TW_ERROR, "error: not implemented yet");
-  return 0;
+TWDEF bool tw_server_start(tw_server_t *server) {
+#ifdef _WIN32
+  WSADATA wsa;
+  if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+    tw_log(TW_ERROR, "WSAStartup failed");
+    return false;
+  }
+#endif
+
+  if ((server->fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    tw_log(TW_ERROR, "Socket failed");
+    return false;
+  }
+
+  server->addr.sin_family = AF_INET;
+  server->addr.sin_addr.s_addr = INADDR_ANY;
+  server->addr.sin_port = htons(TW_DEFAULT_PORT);
+
+  if (bind(server->fd, (struct sockaddr *)&server->addr, sizeof(server->addr)) <
+      0) {
+    tw_log(TW_ERROR, "Socket bind failed");
+    return false;
+  }
+
+  if (listen(server->fd, 10) < 0) {
+    tw_log(TW_ERROR, "Socket listen failed");
+    return false;
+  }
+
+  return true;
 }
+
+TWDEF bool tw_server_stop(tw_server_t *server) {
+  if (close(server->fd) < 0) {
+    return false;
+  }
+
+#ifdef _WIN32
+  WSACleanup();
+#endif
+
+  return true;
+}
+
+TWDEF bool tw_server_accept(tw_server_t *server, tw_conn_t *conn) {
+  socklen_t addr_len = sizeof(conn->addr);
+
+  if ((conn->fd =
+           accept(server->fd, (struct sockaddr *)&conn->addr, &addr_len)) < 0) {
+    tw_log(TW_ERROR, "Accept failed");
+    return false;
+  }
+
+  return true;
+}
+
+TWDEF ssize_t tw_conn_read(tw_conn_t *conn, char *buf, size_t len) {
+  ssize_t bytes_read = recv(conn->fd, buf, len, 0);
+  return bytes_read;
+};
+
+TWDEF ssize_t tw_conn_write(tw_conn_t *conn, const char *buf, size_t len) {
+  ssize_t bytes_sent = send(conn->fd, buf, len, 0);
+  return bytes_sent;
+};
+
+TWDEF void tw_conn_close(tw_conn_t *conn) { close(conn->fd); };
 
 #endif  // THINWIRE_IMPL
